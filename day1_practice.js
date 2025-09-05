8b33e6ab5e6cc6c248ced1c3070ce04d60053258
Nikhil gangajaliya <nikhilgangajaliya5@gmail.com>
2025-09-05 15:21:32 +0530

    Day 1 : practice

---
+++ day1_practice.js
@@ -0,0 +1,9 @@
+console.log("Nikhil Ashokbhai Gangajaliya");
+let a=20;
+let b=10;
+console.log("Total :",a+b);
+console.log("Difference :",a-b);
+console.log("Multiplication :",a*b);
+console.log("Division :",a/b);
+const pi = 3.14159;
+console.log("value of PI :",pi);
