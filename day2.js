0b4758d1d1cf13c615c8e9c484a1a304fedc789b
Nikhil gangajaliya <nikhilgangajaliya5@gmail.com>
2025-09-05 15:56:27 +0530

    Day 2 : Variable & Data Types

---
+++ day2.js
@@ -0,0 +1,30 @@
+var a = 10;
+console.log("var a :",a);
+
+let b = 20;
+console.log("let b :",b);
+
+const c = 12;
+console.log("const c :",c);
+
+var a = 13;
+console.log("var a :",a);
+
+let num = 123;
+let num2 = 123.34;
+let str = "Nikhil";
+let isStudent = true;
+let notDefined;
+let emptyValue = null;
+
+console.log("num : ",num);
+console.log("num2 :",num2);
+console.log("str :",str);
+console.log(typeof notDefined);
+console.log(typeof emptyValue);
+
+let bigNum = 163232342342453448748n;
+console.log("bigNum :",bigNum, typeof bigNum);
+
+
+
