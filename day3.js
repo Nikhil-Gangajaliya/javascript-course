36ff5bed2e8ddc0a23db1dc178bd88150c7df9d9
Nikhil gangajaliya <nikhilgangajaliya5@gmail.com>
2025-09-06 14:35:15 +0530

    Updated

--- day3.js
+++ day3.js
@@ -12,6 +12,7 @@ console.log("Slice (0,4):", str1.slice(0, 4));
 console.log("Replace:", str1.replace("Hello", "Hi"));
 console.log("Includes 'Hell':", str1.includes("Hell"));
 console.log("Split:", "one,two,three".split(","));
+console.log("Index of is : ",str1.indexOf("is"));
 
 // Template literal (backticks)
 let name = "Nikhil";
@@ -19,16 +20,36 @@ let greet = `Welcome, ${name}!`;
 console.log(greet);
 
 // NUMBER EXAMPLES
-let num = 123.4567;
+let num = 123.456;
 
-console.log("Number:", num);
-console.log("toFixed(2):", num.toFixed(2));
-console.log("toString:", num.toString());
+console.log(num.toFixed(2));   // "123.46" (2 decimal places)
+console.log(num.toString());   // "123.456" (converted to string)
+console.log(Number("123"));    // 123
+console.log(parseInt("12px")); // 12
+console.log(parseFloat("12.34")); // 12.34
+
 
 // MATH METHODS
-console.log("Random:", Math.random()); 
-console.log("Floor(5.9):", Math.floor(5.9));
-console.log("Ceil(5.1):", Math.ceil(5.1));
-console.log("Round(5.5):", Math.round(5.5));
-console.log("Max:", Math.max(5, 10, 15));
-console.log("Min:", Math.min(5, 10, 15));
+console.log(Math.round(4.6));   // 5
+console.log(Math.floor(4.9));   // 4
+console.log(Math.ceil(4.1));    // 5
+console.log(Math.pow(2, 3));    // 8
+console.log(Math.sqrt(16));     // 4
+console.log(Math.random());     // random number 0–1
+console.log(Math.floor(Math.random() * 10) + 1); // 1–10
+
+let fname = "Nikhil";
+let lname = "Gangajaliya";
+
+console.log(`${fname} ${lname}`);
+
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log("You rolled: " + dice);
+
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+let password = "";
+for (let i = 0; i < 8; i++) {
+  let randomIndex = Math.floor(Math.random() * chars.length);
+  password += chars[randomIndex];
+}
+console.log("Generated Password:", password);
