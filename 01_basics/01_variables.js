c719f968ba5e4397afd44cddd12f10f046e3e58b
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2025-11-09 11:37:50 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123321;
+let accountEmail = "nikhilgangajaliya5@gmail.com";
+var accountPw = "123"; // don't use this.
+accountCity = "Mumbai"; // also don't use this.
+let accountState;
+
+// accountId = 2222; // const can't change.
+
+accountEmail = "ng@gmail.com";
+accountPw = "111";
+accountCity = "Rajkot";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPw);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPw,accountCity,accountState]); //print in tabular formate
