bc32588f375fcd6cd711e88155241dfce20afe94
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2025-11-09 17:44:20 +0530

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let age = "Nikhil";
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+let valueInNumber = Number(age);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "nikhil" => true
+
+let score = 21;
+
+let stringNumber = String(score);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
