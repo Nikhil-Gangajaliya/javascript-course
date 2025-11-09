6fd76c2803cd990f1db0eff403eec9b12a9080d8
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2025-11-09 12:05:20 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all code as newer version
+
+let name = "Nikhil"; //string
+let age = 20; //number
+let isLoggedIn = true //boolean
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+
+
