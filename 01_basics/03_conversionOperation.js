8c25a14b9ccd2db9ae112530644c7a3e3555ade3
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2025-11-09 18:17:14 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,7 +24,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let score = 21;
 
 let stringNumber = String(score);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// ************ Operatons *************
 
+let value = 71;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello";
+let str2 = " nikhil";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let x = 100;
+let y = ++x;
+
+// console.log(x,y);
+
+let n = 100;
+let d = n++;
+
+// console.log(n,d);
